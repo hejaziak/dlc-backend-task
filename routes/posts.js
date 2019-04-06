507bb8beedd7ecbe86a31ac691dd3ab6d22891f0
AtherHegazy <atherkhalid158@gmail.com>
2019-04-06 23:34:31 +0200

    view recent posts

--- routes/posts.js
+++ routes/posts.js
@@ -21,4 +21,20 @@ router.post('/', (req, res, next) => {
     });
 });
 
+router.get('/recent', (req, res, next) => {
+    // console.log(req.body.mentions)
+    postsController.getRecent(req.body).then(response => {
+        console.log(response)
+        let { error } = response
+        if (error) {
+            let err = createError(401, error);
+            next(err)
+        } else {
+            res.json(response)
+        }
+    }).catch(err => {
+        next(err)
+    });
+});
+
 module.exports = router;
